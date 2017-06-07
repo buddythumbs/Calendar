774091cd3c3549d6e9aafcc76e2598b3890aeec3
buddythumbs <moleary1987@gmail.com>
2017-06-07 21:42:21 +0100

    Adding in styling for events in year view

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,162 @@
 (function() {
-  scheduler.init('scheduler_here', new Date(),"year");
   scheduler.locale.labels.year_tab ="Year";
+  scheduler.config.xml_date="%Y-%m-%d %H:%i";
+  scheduler.config.details_on_dblclick = true;
+  scheduler.config.details_on_create = true;
+  scheduler.config.active_link_view = "month";
+
+
   var events = [
-{id:1, text:"Meeting",   start_date:"04/11/2017 14:00",end_date:"04/11/2017 17:00",type:"shift"},
-{id:2, text:"Conference",start_date:"04/15/2017 12:00",end_date:"04/18/2017 19:00",type:"vacation"},
-{id:3, text:"Interview", start_date:"04/24/2017 09:00",end_date:"04/24/2017 10:00",type:"vacation"},
-{id:1, text:"Meeting",   start_date:"05/11/2017 14:00",end_date:"05/11/2017 17:00",type:"shift"},
-{id:2, text:"Conference",start_date:"05/15/2017 12:00",end_date:"05/18/2017 19:00",type:"vacation"},
-{id:3, text:"Interview", start_date:"05/24/2017 09:00",end_date:"05/24/2017 10:00",type:"vacation"},
-{id:1, text:"Meeting",   start_date:"05/25/2017 14:00",end_date:"05/26/2017 17:00",type:"vacation"},
-{id:2, text:"Conference",start_date:"05/26/2017 20:00",end_date:"05/26/2017 22:00",type:"shift"},
-{id:3, text:"Interview", start_date:"05/27/2017 09:00",end_date:"05/27/2017 10:00",type:"shift"}
-];
-scheduler.templates.event_class = function (start, end, event) {
-    if (event.type == 'shift') return "shift_event";
-    return "vacation_event"; 
-};
-scheduler.parse(events, "json");//takes the name and format of the data source
+  {
+    id:1,
+    text:"MOLE",
+    start_date:"2017-05-10 14:00",
+    end_date:"2017-05-10 17:00",
+    type:"shift",
+    status:"requested"
+  },{
+    id:2,
+    text:"GOGR",
+    start_date:"2017-05-10",
+    end_date:"2017-05-13",
+    type:"vacation",
+    status:"approved"
+  },{
+    id:3,
+    text:"RYSW",
+    start_date:"2017-05-11",
+    end_date:"2017-05-14",
+    type:"vacation",
+    status:"approved"
+  },{
+    id:4,
+    text:"EOGO",
+    start_date:"2017-05-08",
+    end_date:"2017-05-13",
+    type:"vacation",
+    status:"approved"
+  },{
+    id:5,
+    text:"A",
+    start_date:"2017-04-29 19:00",
+    end_date:"2017-05-02 07:00",
+    type:"shift",
+    shift:"night",
+  },{
+    id:6,
+    text:"B",
+    start_date:"2017-04-30",
+    end_date:"2017-05-04",
+    type:"shift",
+    shift:"day",
+  },{
+    id:7,
+    text:"D",
+    start_date:"2017-04-26",
+    end_date:"2017-04-30",
+    type:"shift",
+    shift:"day",
+  },{
+    id:8,
+    text:"C",
+    start_date:"2017-04-26",
+    end_date:"2017-04-29",
+    type:"shift",
+    shift:"night",
+  }];
+  scheduler.init('scheduler_here', new Date(),"year");
+  scheduler.templates.event_class = function (start, end, event) {
+    var Class = ""
+    switch (event.type) {
+      case 'shift':
+        if (event.shift == 'night') return "shift";
+        Class = "shift";
+        break;
+      case 'vacation':
+        if (event.status == 'requested') Class = "requested_req";
+        Class = "approved_req";
+        break;
+      default:
+      var Class = 'shift'
+    }
+    return Class
+  };
+  scheduler.templates.year_tooltip = function(start,end,event) {
+    switch (event.type) {
+      case 'shift':
+        var tooltip = (event.shift == 'night'?
+          " <span class='zmdi zmdi-star text-muted'></span> ":
+          " <span class='zmdi zmdi-sun text-warning'></span> ") +
+          event.text + " " + event.shift
+        break;
+      case 'vacation':
+      var tooltip = (event.status == 'approved'?
+        " <span class='zmdi zmdi-check text-success'></span> ":
+        " <span class='zmdi zmdi-square-o text-primary'></span> ") +
+        event.text
+        break;
+      default:
+      var tooltip = event.text
+    }
+    return tooltip
+  };
+  scheduler.templates.event_bar_text = function(start,end,event) {
+    switch (event.type) {
+      case 'shift':
+        var bar = event.text + " " + event.shift + " " + event.type
+        break;
+      case 'vacation':
+      var bar =  event.text + " " + event.status + " " + event.type
+        break;
+      default:
+      var bar = event.text
+    }
+    return bar
+  };
+  scheduler.parse(events, "json");//takes the name and format of the data source
+  // Custom form for lightbox
+  var html = function(id) { return document.getElementById(id); }; //just a helper
+
+  scheduler.showLightbox = function(id) {
+  	var ev = scheduler.getEvent(id);
+  	scheduler.startLightbox(id, html("my_form"));
+
+  	html("description").focus();
+  	html("description").value = ev.text;
+  	html("from").value = ev.start_date || "";
+  	html("to").value = ev.end_date || "";
+    $('.flatpickr').flatpickr({
+        enableTime: true,
+        time_24hr: true
+    })
+  };
+
+  function save_form() {
+  	var ev = scheduler.getEvent(scheduler.getState().lightbox_id);
+  	ev.text = html("description").value;
+  	ev.start_date = html("from").value;
+  	ev.end_date = html("to").value;
+
+  	scheduler.endLightbox(true, html("my_form"));
+  }
+  function close_form() {
+  	scheduler.endLightbox(false, html("my_form"));
+  }
+
+  function delete_event() {
+  	var event_id = scheduler.getState().lightbox_id;
+  	scheduler.endLightbox(false, html("my_form"));
+  	scheduler.deleteEvent(event_id);
+  }
+
+  $(document).on('click','#save',function () {
+    save_form()
+  })
+  $(document).on('click','#close',function () {
+    close_form()
+  })
+  $(document).on('click','#delete',function () {
+    delete_event()
+  })
+
 })()
